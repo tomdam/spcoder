984194cc175948e208fbbdc16bb6ee83e116492a
Damjan Tomic <259436+tomdam@users.noreply.github.com>
2022-09-10 23:01:39 +0200

    Creating the backup of json flow definition before saving it.

--- src/SPCoder.PowerPlatform/Utils/PPFlowJsonEditedFile.cs
+++ src/SPCoder.PowerPlatform/Utils/PPFlowJsonEditedFile.cs
@@ -3,6 +3,7 @@ using SPCoder.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,7 @@ namespace SPCoder.PowerPlatform.Utils
         {
             try
             {
+                SaveBackupToDisk();
                 string editedJson = new StreamReader(this.Stream).ReadToEnd();
                 FlowJson["clientdata"] = editedJson;
                 SPCoderLogger.Logger.LogInfo("Updated flow clientdata localy! Use \"Update cloud flow\" action to push the changes to server.");
@@ -31,6 +33,27 @@ namespace SPCoder.PowerPlatform.Utils
             }
 
             return true;
+        }
+
+        private void SaveBackupToDisk()
+        {
+            try
+            {
+                //check if the backup folder exists
+                string backupFolder = Path.Combine(Environment.CurrentDirectory, "Flows/Backup", FlowJson["workflowid"].ToString());
+
+                DirectoryInfo di = new DirectoryInfo(backupFolder);
+                if (!di.Exists)
+                {
+                    di.Create();                    
+                }
+                string fileName = Regex.Replace(DateTime.Now.ToString("o"), @"[+T:\.]", "") + ".json";
+                File.WriteAllText(Path.Combine(di.FullName, fileName), FlowJson["clientdata"].ToString());
+            }
+            catch (Exception exc)
+            {
+                SPCoderLogger.Logger.LogError("Error while saving the backup of the flow localy." + exc.Message);
+            }
         }
     }
 }
